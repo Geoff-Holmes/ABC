e09bfae47c9d9081e1951e586e64ae54fa7fec87
Geoff Holmes <geoffrholmes@gmail.com>
2013-11-06 21:55:34 +0000

    new model

---
+++ models/Drift_Levy_Diffusion.m
@@ -0,0 +1,44 @@
+function out = Levy_Diffusion(params, metaData)
+
+% out = Levy_Diffusion(params, metaData)
+
+paramNames = {'power', 'scale', 'drift'};
+if nargin == 0
+    out = paramNames;
+else
+    assert(length(paramNames) == length(params))
+    
+    pwr = params(1);
+    scl = params(2);
+    drift = params(3);
+    x = metaData.initial;
+    if size(x,1) > size(x,2), x = x'; end
+    N = length(x);
+    dt = metaData.timeInc;
+    
+    % arbitrary min step length
+    xmin = .1;
+    % max cut off
+    xmax = 10*scl;
+    
+    for t = 2:metaData.T
+        for n = 1:N
+            s = xmax + 1;
+            while s > xmax
+                u = rand();
+                % inverse cdf for power law distribution
+                s = xmin * (1 - u)^(-1/pwr);
+            end
+            % get direction
+            q = rand() * 2 * pi;
+            % incorporate angle to get x,y coords in complex form
+            s = scl * s * exp(q*1i);
+
+            % update
+            x(t, n) = x(t-1, n) + s + drift;
+            if real(x(t, n)) < 0, x(t, n) = x(t, n) - real(x(t, n)); end
+        end
+    end
+    
+    out = x;
+end
